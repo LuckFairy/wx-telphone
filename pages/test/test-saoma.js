a656e711bc54e12379570cd2632f098a9a7964cf
li_amy <2467393405@qq.com>
2018-01-17 14:12:29 +0800

    扫码定位测试

---
+++ pages/test/test-saoma.js
@@ -0,0 +1,80 @@
+// pages/test/test-saoma.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+  saoma(){
+    wx.scanCode({
+      success:(res)=>{
+        console.log('扫码参数res',res);
+      }
+    })
+  },
+  dingwei(){
+    wx.getLocation({
+      type: 'wgs84',
+      success: function (res) {
+        console.log('获取位置参数res',res);
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
